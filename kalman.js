c9cd9371b39c8f9997446640867f2ac14087eae4
Nathaniel Foster <nathanielsmusic@gmail.com>
2015-02-24 14:57:29 -0500

    removed internal variable of predicted state, accessing directly now

--- kalman.js
+++ kalman.js
@@ -26,7 +26,7 @@ var Kalman = {
 
 KalmanModel = (function(){
 
-  function KalmanModel(x_0,P_0,F_k,B_k,Q_k){
+  function KalmanModel(x_0, P_0, F_k, B_k, Q_k){
     this.x_k  = x_0;
     this.P_k  = P_0;
     this.F_k  = F_k;
@@ -39,23 +39,33 @@ KalmanModel = (function(){
     this.P_k_ = this.P_k;
 
     //Predict
-    this.x_k_k_ = this.F_k.x(this.x_k_).add(this.B_k.x(u_k));
-    this.P_k_k_ = this.F_k.x(this.P_k_.x(this.F_k.transpose())).add(this.Q_k);
+    // this.x_k_k_ = this.F_k.x(this.x_k_).add(this.B_k.x(u_k));
+    this.x_k = this.F_k.x(this.x_k_).add(this.B_k.x(u_k));
+    // this.P_k_k_ =
+    //    this.F_k.x(this.P_k_.x(this.F_k.transpose())).add(this.Q_k);
+    this.P_k = this.F_k.x(this.P_k_.x(this.F_k.transpose())).add(this.Q_k);
   }
 
   KalmanModel.prototype.update =  function(o){
     this.I = Matrix.I(this.P_k.rows());
-    //init
+
+    /* init */
+
     this.x_k_ = this.x_k;
     this.P_k_ = this.P_k;
 
-    //update
-    this.y_k = o.z_k.subtract(o.H_k.x(this.x_k_k_));//observation residual
-    this.S_k = o.H_k.x(this.P_k_k_.x(o.H_k.transpose())).add(o.R_k);//residual covariance
-    console.log(this.P_k_k_,o.H_k,this.S_k)
-    this.K_k = this.P_k_k_.x(o.H_k.transpose().x(this.S_k.inverse()));//Optimal Kalman gain
-    this.x_k = this.x_k_k_.add(this.K_k.x(this.y_k));
-    this.P_k = this.I.subtract(this.K_k.x(o.H_k)).x(this.P_k_k_);
+    /* update */
+
+    //observation residual
+    this.y_k = o.z_k.subtract(o.H_k.x(this.x_k_));
+
+    //residual covariance
+    this.S_k = o.H_k.x(this.P_k_.x(o.H_k.transpose())).add(o.R_k);
+
+    //Optimal Kalman gain
+    this.K_k = this.P_k_.x(o.H_k.transpose().x(this.S_k.inverse()));
+    this.x_k = this.x_k_.add(this.K_k.x(this.y_k));
+    this.P_k = this.I.subtract(this.K_k.x(o.H_k)).x(this.P_k_);
   }
 
   return KalmanModel;
